3320abf0ac44d480fc4fe5919f41aec79248b95a
Jessica Bee <bee.jess@gmail.com>
2019-01-18 01:28:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Code your solution in this file!
 const startingPoint = 42
+const feetInBlock = 264
 
 function distanceFromHqInBlocks(block) {
   if (block < startingPoint) {
     return startingPoint - block
   } else {
     return block - startingPoint
+  }
+}
+function distanceFromHqInFeet(block) {
+  if (block < startingPoint) {
+    return (startingPoint - block) * 264
   }
 }
