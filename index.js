52ad3c538380a9514cc8e6f87456baac1baff6ad
Jessica Bee <bee.jess@gmail.com>
2019-01-18 01:46:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,7 @@ function calculatesFarePrice(startingBlock, endingBlock) {
   const feetTraveled = distanceTravelledInFeet(startingBlock, endingBlock)
   if (feetTraveled <= 400) {
     return 0
+  } else if (400 < feetTraveled < 2000) {
+    return (feetTraveled - 400) * 0.02
   }
 }
